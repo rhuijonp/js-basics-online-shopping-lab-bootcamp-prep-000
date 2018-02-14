987831afdf8bb7399e7b5dd26d63cd53d39e9350
Rafael Huijon <rhuijonp@gmail.com>
2018-02-14 15:43:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,41 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+ var itemPrice = Math.floor(Math.random() * 100) + 1;
+ //var item = Object.assign({}, {[itemName]: itemPrice});
+ cart.push( Object.assign({[itemName]: itemPrice}));
+ console.log(itemName + " has been added to your cart.");
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length > 1) {
+		var cartStatus = "In your cart, you have ";
+		var and = "and";
+		var at = "at";
+		var totalItems = cart.length;
+		
+		for (var i = 0; i < cart.length; i++) { 
+		var obj = cart[i];
+		var objName = Object.keys(obj); 
+		var objValue = cart[i][objName]; 
+		
+			if (totalItems === 1) {
+				cartStatus += `${objName} ${at} $${objValue}.`; 
+			} else {
+				if (i < (cart.length - 1)) {
+					cartStatus += `${objName} ${at} $${objValue}, `;
+				} else {
+					cartStatus +=`and ${objName} ${at} $${objValue}.`;
+				}
+			}
+		} 
+	} else {
+		cartStatus = "Your shopping cart is empty.";
+	}
+	return cartStatus;
 }
 
 function total() {
