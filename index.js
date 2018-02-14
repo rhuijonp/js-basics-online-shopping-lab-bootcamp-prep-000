14eade235178336cc666cfeb86f89c20644d06bd
Rafael Huijon <rhuijonp@gmail.com>
2018-02-14 17:59:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ function placeOrder(cardNumber){
 	  //cart = [];
 	  console.log("Sorry, we don't have a credit card on file for you.");
 	} else {
-	  console.log(`Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`)
+	  console.log(`Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`);
 	  cart = [];
 	  //console.log("Sorry, we don't have a credit card on file for you.");
 	}
