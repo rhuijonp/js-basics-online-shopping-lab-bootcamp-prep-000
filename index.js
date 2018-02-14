187d54da8a5ce033a34a03748267e53ec598846e
Rafael Huijon <rhuijonp@gmail.com>
2018-02-14 18:25:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ function placeOrder(cardNumber){
   // write your code here
 	var cartTotal = total();
 
-	if ((cardNumber !== "") && (cardNumber !== null && (cardNumber !== 0))){
+	if ((cardNumber !== "") && (cardNumber !== null)){
 	  console.log(`Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`)
 	  cart = [];
 	} else {
