2a3c6a057665cd2f6396c749b4ca1d0950c6e46e
Rafael Huijon <rhuijonp@gmail.com>
2018-02-14 15:46:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,31 +19,44 @@ function addToCart(itemName) {
 
 function viewCart() {
   // write your code here
-  if (cart.length > 1) {
+ if (cart.length >= 1) {
 		var cartStatus = "In your cart, you have ";
-		var and = "and";
-		var at = "at";
 		var totalItems = cart.length;
 		
 		for (var i = 0; i < cart.length; i++) { 
-		var obj = cart[i];
-		var objName = Object.keys(obj); 
-		var objValue = cart[i][objName]; 
+			var obj = cart[i];
+			var objName = Object.keys(obj); 
+			var objValue = cart[i][objName]; 
 		
-			if (totalItems === 1) {
-				cartStatus += `${objName} ${at} $${objValue}.`; 
-			} else {
-				if (i < (cart.length - 1)) {
-					cartStatus += `${objName} ${at} $${objValue}, `;
+		  // Cart has only 1 item
+			if (totalItems === 1){
+				cartStatus += `${objName} ${at} $${objValue}.`;
+			}
+			
+			// Cart has two items	
+			if (totalItems === 2) {
+				if (i === 0) {
+					cartStatus += `${objName} ${at} $${objValue}`;
 				} else {
-					cartStatus +=`and ${objName} ${at} $${objValue}.`;
+					cartStatus += ` and ${objName} ${at} $${objValue}.`;
 				}
 			}
+			
+			// Cart has 3 or more items
+			if (totalItems > 2) {
+				if (totalItems > (i + 1)) {
+					cartStatus += ` ${objName} ${at} $${objValue},`;
+				} else {
+				  // this is the last item in the cart
+					cartStatus +=  ` and ${objName} ${at} $${objValue}.`;
+				} 
+					
+			}	
 		} 
 	} else {
 		cartStatus = "Your shopping cart is empty.";
 	}
-	return cartStatus;
+	console.log(cartStatus);
 }
 
 function total() {
